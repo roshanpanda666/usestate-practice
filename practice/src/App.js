48f6d1708cbcb24633f6db0060f404070f30b4bc
Roshan panda <roshanpanda666@gmail.com>
2023-02-16 11:09:01 +0530

    commiting

---
+++ practice/src/App.js
@@ -0,0 +1,16 @@
+import React,{useState} from 'react'
+
+const App = () => {
+  const click=()=>{
+    setText("clicked!!!!!!!!!")
+  }
+  const[Text,setText]=useState("not changed yet!")
+  return (
+    <div>
+      <h1>{Text}</h1>
+      <button onClick={click}>click me</button>
+    </div>
+  )
+}
+
+export default App
